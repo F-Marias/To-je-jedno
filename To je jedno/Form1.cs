18a1874c23653d02f6a59487f3fdd7711763b195
Filip Mariáš <filipmarias01@zak.spsul.cz>
2022-09-14 10:22:24 +0200

    Add project files.

---
+++ To je jedno/Form1.cs
@@ -0,0 +1,16 @@
+namespace To_je_jedno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int hlava = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
